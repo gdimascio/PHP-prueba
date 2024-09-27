e07a2840988bf5d7552ea714ca12958fe2279ef6
gdimascio <guido.dimascio@gmail.com>
2024-09-27 11:14:39 -0300

    separo la logica para revisar datos vacios y corrijo errores sintacticos

--- process.php
+++ process.php
@@ -1,28 +1,30 @@
 <?php
     require 'config.php';
-    
+
     // recibir los datos
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $mensaje = $_POST['mensaje'];
 
-    if (!empty($nombre) && !empty($email) && !empty($mensaje)) {
-        // preparar la consulta SQL
-        $stmt = $conn->prepare("INSERT INTO mensajes (nombre, email, mensaje) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $nombre, $email, $mensaje);
-
-        // ejecutar la consulta
-        if ($stmt->execute()) {
-            echo "Datos guardados!";
-        } else {
-            echo "Error al guardar los datos: " . $conn->erro;
-        }
-
-        // cerrar declaracion
-        $stmt->close();
-    } else {
-        echo "Formulario incompleto.";
+    // comprobar datos vacios
+    if (empty($nombre) || empty($email) || empty($mensaje)) {
+        die "Formulario incompleto.";
     }
+
+    // preparar la consulta SQL
+    $query = "INSERT INTO mensajes (nombre, email, mensaje) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $nombre, $email, $mensaje);
+
+    // ejecutar la consulta
+    if ($stmt->execute()) {
+        echo "Datos guardados!";
+    } else {
+        echo "Error al guardar los datos: " . $conn->error;
+    }
+
+    // cerrar declaracion
+    $stmt->close();
 
     $conn->close();
 ?>
